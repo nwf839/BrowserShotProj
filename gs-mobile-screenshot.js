17847c4b162109cce0230455bf58bd844045375e
nwf839 <CactusJ43@gmail.com>
2017-11-24 16:11:56 -0500

    Added cnet demo that loads page and scrolls viewport

--- gs-mobile-screenshot.js
+++ gs-mobile-screenshot.js
@@ -6,10 +6,13 @@ const devices = require('puppeteer/DeviceDescriptors');
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    let wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 
     await page.emulate(devices['iPhone 6']);
     await page.goto('https://www.gamespot.com', {"waitUntil" : "networkidle", "networkIdleTimeout" : 3000});
     await page.screenshot({ path: 'screenshots/gamespot-mob.png', fullPage: true});
+    await wait(15000);
+    await page.screenshot({ path: 'screenshots/gamespot-mob2.png', fullPage: true});
     await browser.close();
 })();
 
